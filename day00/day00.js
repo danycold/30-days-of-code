0781c805da52dddb895a311467a80b477365ffe3
Denis <dashevchenko93@gmail.com>
2017-01-12 00:46:08 +0300

    day00 on js

---
+++ day00/day00.js
@@ -0,0 +1,10 @@
+/*global document, window, alert, console, require*/
+
+function processData(inputString) {
+    'use strict';
+    // This line of code prints the first line of output
+    console.log("Hello, World.");
+
+    // Write the second line of output that prints the contents of 'inputString' here.
+    console.log(inputString);
+}
